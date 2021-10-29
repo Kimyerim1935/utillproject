c04dff5676f8515d5a2c97bc50516b353ec7c846
kimyerim1935 <kimyerim1935@naver.com>
2021-10-29 19:19:48 +0900

    체크한 항목 추가/삭제

--- src/Utill/DataGrid/SingleDataTable.js
+++ src/Utill/DataGrid/SingleDataTable.js
@@ -1,33 +1,42 @@
 import React from 'react';
+import Delete from '../../assets/img/delete.png';
 
 const SingleDataTable = (props) => {
-    const {title, data, selectList, openModal, noticeData} = props;
+    const {title, data, selectList, openModal, changeData, teamList, deleteTeam} = props;
+
+    const itemClass = (id) => {
+        if (id === 'textArea') {
+            return 'textArea'
+        } else if (id === 'modal') {
+            return 'teamArea'
+        } else return 'list'
+    }
 
     return (
-         <>
-            <span className="single_dataTitle ">{title}</span>
+        <>
+            <span className="single_dataTitle">{title}</span>
             <ul className="single_dataTable">
                 {
                     data.map((item,idx)=> {
                         return (
-                            <li key={idx} className={item.id === 'textArea' ? "textArea" : 'list'}>{item.title}
+                            <li key={idx || data.type} className={itemClass(item.id)}>{item.title}
                                 {
                                     (item.id === "input" ) && 
-                                        <span><input/></span>
+                                        <span><input onChange={(e)=>changeData(e.target.value, data[idx].type)}/></span>
                                 }
                                 {
                                     (item.id === "textArea" ) && 
-                                        <span><textarea /></span>
+                                        <span><textarea onChange={(e)=>changeData(e.target.value, data[idx].type)}/></span>
                                 }
                                 {
                                     (item.id === "select" ) && 
                                     <span>
-                                        <select>
-                                            <option key={-1}>전체</option>
+                                        <select onChange={(e)=>changeData(e.target.value, data[idx].type)}>
+                                            <option key={-1} value={-1}>전체</option>
                                             {
                                                 selectList.map((item)=> {
                                                     return (
-                                                        <option key={item.id}>{item.title}</option>
+                                                        <option key={item.id} value={item.id}>{item.title}</option>
                                                     )
                                                 })
                                             }
@@ -37,6 +46,24 @@ const SingleDataTable = (props) => {
                                 {
                                     (item.id === "modal" ) && 
                                         <span>
+                                            {
+                                                teamList && 
+                                                teamList.map((item, idx) => {
+                                                    return <span style={{display:'block'}} key={idx}>{Object.values(item)}<img src={Delete} alt="icon" onClick={()=>deleteTeam(idx, item)}/></span>
+
+                                                    // return(
+                                                    //     item[idx] ?
+                                                    //         <span key={idx}>
+                                                    //             { 
+                                                    //                 item.map((team) => {
+                                                    //                     return Object.values(team.name)
+                                                    //                 })
+                                                    //             }
+                                                    //             <img src={Delete} alt="icon" onClick={()=>deleteTeam(idx, item)}/>
+                                                    //         </span>
+                                                    // ) 
+                                                })
+                                            }
                                             <button 
                                                 className="pagebutton"
                                                 onClick={()=>openModal()}>내 학생 리스트</button>

2266dfceb84541640d3862f0bc0b4036497f8474
kimyerim1935 <kimyerim1935@naver.com>
2021-11-11 14:42:40 +0900

    테이블 추가

--- src/Utill/CheckList/CheckList.js
+++ src/Utill/CheckList/CheckList.js
@@ -1,13 +1,36 @@
 import React, {useEffect, useState} from 'react';
-import DataTable from '../DataGrid/DataTable';
-import SingleDataTable from '../DataGrid/SingleDataTable';
-import {StudentModal} from '../Modal/Modal';
-// import {handleListPage, handleconfirm} from '../../utill/utill';
+import DataTable from '../commons/DataTable';
+import SingleDataTable from '../commons/SingleDataTable';
+import {StudentModal} from '../../utill/Modal';
+import {handleListPage, handleconfirm} from '../../utill/utill';
 
 const ClassProduce = () => {
 
+    const managerInfo = {
+        name : '김예림',
+        grade: '2/9',
+        id: 'yerim98'
+    }
+    const classData = [
+        { id: 'input',title: '클래스명', type: 'className' },
+        { title: '담당자', info: managerInfo.name },
+        { title: '학년/반', info: managerInfo.grade  },
+        { title: '담당자 아이디', info: managerInfo.id  },
+    ]
+
     const subjectData = [
+        { id: 'input', title: '과업명', type: 'subjectName'},
+        { id: 'textArea', title: '과업내용', type: 'subjectContents'},
+        { id: 'select', title: '과업선택', type: 'selectSubject'},
         { id: 'modal', title: '조편성', type: 'createteam'},
+    ]
+
+    const selectData = [
+        { id: 1, title: '지하철 불끄기', subjectName: '지하철 불끄기' , subjectContents: '지하철 불 끕니다'},
+        { id: 2, title: '개찰구 불끄기', subjectName: '개찰구 불끄기' , subjectContents: '개찰구 불 끕니다'},
+        { id: 3, title: '승강장 불끄기', subjectName: '승강장 불끄기' , subjectContents: '승강장 불 끕니다'},
+        { id: 4, title: '역무원실 불끄기', subjectName: '역무원실 불끄기' , subjectContents: '역무원실 불 끕니다'},
+        { id: 5, title: '화장실 불끄기', subjectName: '화장실 불끄기' , subjectContents: '화장실 불 끕니다'},
     ]
 
     const studentList_columns = [
@@ -137,75 +160,81 @@ const ClassProduce = () => {
     const [checked, setChecked] = useState(false);
 
     const [selectItem, setSelectItem] = useState([]);
-
-    const [newClass, setNewClass] = useState({
-            createTeam: [
-                [
-                    {   
-                        id: 1,
-                        'name': 'name0',
-                        'userid': 'id_00',
-                        'class': '2/9',
-                        'select': ''
-                    },
-                    {
-                        id: 2,
-                        'name': 'name1',
-                        'userid': 'id_01',
-                        'class': '2/9',
-                        'select': ''
-                    },
-                    {
-                        id: 3,
-                        'name': 'name2',
-                        'userid': 'id_03',
-                        'class': '2/9',
-                        'select': ''
-                    },
-                    {
-                        id: 4,
-                        'name': 'name3',
-                        'userid': 'id_04',
-                        'class': '2/9',
-                        'select': ''
-                    },
-                ],
-                [
-                    {
-                        id: 5,
-                        'name': 'name4',
-                        'userid': 'id_05',
-                        'class': '2/9',
-                        'select': ''
-                    },
-                    {
-                        id: 6,
-                        'name': 'name5',
-                        'userid': 'id_06',
-                        'class': '2/9',
-                        'select': ''
-                    },
-                    {
-                        id: 7,
-                        'name': 'name6',
-                        'userid': 'id_07',
-                        'class': '2/9',
-                        'select': ''
-                    }, 
-                    {
-                        id: 8,
-                        'name': 'name7',
-                        'userid': 'id_08',
-                        'class': '2/9',
-                        'select': ''
-                    }, 
-                ],
-            ]
-        });
-
+    const [className,setClassName] = useState('');
+    
+    const [newClass, setNewClass] = useState([
+            {
+                subjectName: '',
+                subjectContents: '',
+                selectSubject : -1,
+                createTeam: [
+                    [
+                        {   
+                            id: 1,
+                            'name': 'name0',
+                            'userid': 'id_00',
+                            'class': '2/9',
+                            'select': ''
+                        },
+                        {
+                            id: 2,
+                            'name': 'name1',
+                            'userid': 'id_01',
+                            'class': '2/9',
+                            'select': ''
+                        },
+                        {
+                            id: 3,
+                            'name': 'name2',
+                            'userid': 'id_03',
+                            'class': '2/9',
+                            'select': ''
+                        },
+                        {
+                            id: 4,
+                            'name': 'name3',
+                            'userid': 'id_04',
+                            'class': '2/9',
+                            'select': ''
+                        },
+                    ],
+                    [
+                        {
+                            id: 5,
+                            'name': 'name4',
+                            'userid': 'id_05',
+                            'class': '2/9',
+                            'select': ''
+                        },
+                        {
+                            id: 6,
+                            'name': 'name5',
+                            'userid': 'id_06',
+                            'class': '2/9',
+                            'select': ''
+                        },
+                        {
+                            id: 7,
+                            'name': 'name6',
+                            'userid': 'id_07',
+                            'class': '2/9',
+                            'select': ''
+                        }, 
+                        {
+                            id: 8,
+                            'name': 'name7',
+                            'userid': 'id_08',
+                            'class': '2/9',
+                            'select': ''
+                        }, 
+                    ],
+                ]
+            }
+        ]);
     const [openModal, setOpenModal] = useState(false);
     const [teamList, setTeamList] = useState([]);
- 
+    const [index,setIndex] = useState();
+
     useEffect(() => {
         handleFilter()
     },[])
@@ -222,26 +251,36 @@ const ClassProduce = () => {
 
     useEffect(()=>{
         setOpenModal(false);
-        setTeamList(newClass.createTeam)
-    },[newClass.createTeam])
+        for (let i = 0; i < newClass.length; i++) {
+            setTeamList(newClass[i]?.createTeam)
+        }
+    },[newClass])
 
+    useEffect(()=> {
+        
+    },[teamList])
+
+    console.log('new',newClass)
     const handleFilter = () => {
         let newStudent = studentList_rows.slice();
 
-        if (newClass.createTeam.length !== 0) {
-            newClass.createTeam.map((i)=> {
-                return i.map((l)=> {
-                    return newStudent = newStudent.filter((item)=> {
-                        return item.name !== l.name
+        for (let i = 0; i < newClass.length; i++) {
+            if (newClass[i]?.createTeam.length !== 0) {
+                newClass[i]?.createTeam.map((t) => {
+                    return t.map((l) => {
+                        return newStudent = newStudent.filter((item)=> {
+                            return item.name !== l.name
+                        })
                     })
                 })
-            })
+            }
         }
         setStudentList_rows(newStudent)
     }
 
-    const handleModal = () => {
+    const handleModal = (index) => {
         setOpenModal(true);
+        setIndex(index)
     }
 
     const handleCloseModal = () => {
@@ -251,6 +290,62 @@ const ClassProduce = () => {
                 setOpenModal(false)
             }
         } else setOpenModal(false)
+    }
+    const handleClassName = (data) => {
+        setClassName(data)
+    }
+
+    const handleAdd = () => {
+        setNewClass(prev => [...prev,
+            {
+                subjectName: '',
+                subjectContents: '',
+                selectSubject : -1,
+                createTeam: [
+                ]
+            }
+        ])
+    }
+
+    const handleClassContents = (data, type, index) => {
+        switch (type) {
+            case 'subjectName': {
+                setNewClass(prev => prev.map((nn, i) => {
+                    if (i === index) {
+                        nn.subjectName = data
+                    } 
+                    return nn
+                }))
+                break;
+            }
+            case 'subjectContents': {
+                setNewClass(prev => prev.map((nn, i) => {
+                    if (i === index) {
+                        nn.subjectContents = data
+                    } 
+                    return nn
+                }))
+                break;
+            }
+            case 'selectSubject': {
+                setNewClass(prev => prev.map((nn, i) => {
+                    if (i === index) {
+                        nn.selectSubject = data;
+                        for(let i = 0; i < selectData.length; i++) {
+                            if (selectData[i].id === Number(data)) {
+                                nn.subjectName = selectData[i].subjectName;
+                                nn.subjectContents =  selectData[i].subjectContents;
+                            }
+                        }
+                    } 
+                    return nn
+                }))
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 
     const handleCheck = ({target}) => {
@@ -262,6 +357,10 @@ const ClassProduce = () => {
         if (checked) {
             selectItem.push(id)
             setSelectItem(selectItem);
+            // if (selectItem.length > 5) {
+            //     alert('최대 5명까지만 선택 가능합니다.')
+            //     selectItem.pop(id);
+            // }
         } else if (!checked && selectItem.includes(id)) {
             selectItem.pop(id);
             setSelectItem(selectItem);
@@ -288,9 +387,9 @@ const ClassProduce = () => {
 
                 setSelectItem([]);
 
-                newClass.createTeam.push(newTeam)
+                newClass[index].createTeam.push(newTeam)
 
-                newClass.createTeam.map((i)=> {
+                newClass[index].createTeam.map((i)=> {
                     return i.map((l)=> {
                         return newStudent = newStudent.filter((item)=> {
                             return item.name !== l.name
@@ -305,8 +404,8 @@ const ClassProduce = () => {
         }
     }
 
-    const handleClassDelete = (idx, item) => {
-        const deleteItem = newClass.createTeam;
+    const handleClassDelete = (idx, item, index) => {
+        const deleteItem = newClass[index].createTeam;
 
         if (deleteItem.includes(item)) {
             deleteItem.splice(idx, 1)
@@ -316,23 +415,46 @@ const ClassProduce = () => {
             return studentList_rows.push(data)
         })
         
-        setNewClass({
-            createTeam : deleteItem
-        })
+        setNewClass(prev => prev.map((nn, i) => {
+            if (i === index) {
+                nn.createTeam = deleteItem
+            } 
+            return nn
+        }))
         setOpenModal(false);
     }
 
     return (
         <div className="noneContainerWrapper">
-            <span className="title">■ 체크리스트</span>
-            
-            <SingleDataTable 
-                title="체크리스트 항목"
-                data={subjectData}
-                teamList={teamList}
-                openModal={handleModal}
-                deleteTeam={handleClassDelete}
+            <span className="title">■ 클래스 상세</span>
+            <DataTable
+                title="클래스 기본정보"
+                data={classData}
+                newClass={handleClassName}
             />
+            <span className="single_dataTitle">
+                과업 정보 
+                <button onClick={handleAdd}>과업 추가</button>
+            </span>
+            
+            {
+                newClass?.map((cc, idx) => {
+                    return (
+                        <SingleDataTable 
+                            index={idx}
+                            value={cc}
+                            data={subjectData}
+                            selectList={selectData}
+                            teamList={cc.createTeam}
+                            changeData={handleClassContents}
+                            openModal={handleModal}
+                            deleteTeam={handleClassDelete}
+                        />
+                    ) 
+                   
+                })
+               
+            }
             {
                 openModal && 
                 <StudentModal 
@@ -344,14 +466,14 @@ const ClassProduce = () => {
                     createTeam={handleClassCreate}
                     />
             }
-            {/* <button 
+            <button 
                 className="listbutton"
                 onClick={()=>handleconfirm('/Class', '해당클래스를 생성하시겠습니까?')}
                 >생성</button>
             <button 
                 className="list_backButton"
                 onClick={()=>handleListPage('/Class')}
-            >뒤로</button> */}
+            >뒤로</button>
         </div>
     )
 }
--- src/Utill/DataGrid/SingleDataTable.js
+++ src/Utill/DataGrid/SingleDataTable.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Delete from '../../assets/img/delete.png';
 
 const SingleDataTable = (props) => {
-    const {title, data, selectList, openModal, changeData, teamList, deleteTeam} = props;
+    const {index, value, title, data, selectList, openModal, changeData, teamList, deleteTeam} = props;
 
     const itemClass = (id) => {
         if (id === 'textArea') {
@@ -11,28 +11,36 @@ const SingleDataTable = (props) => {
             return 'teamArea'
         } else return 'list'
     }
-
+    console.log('value',value)
     return (
         <>
             <span className="single_dataTitle">{title}</span>
             <ul className="single_dataTable">
                 {
                     data.map((item,idx)=> {
+                        console.log(item)
                         return (
                             <li key={idx || data.type} className={itemClass(item.id)}>{item.title}
                                 {
                                     (item.id === "input" ) && 
-                                        <span><input onChange={(e)=>changeData(e.target.value, data[idx].type)}/></span>
+                                        <span>
+                                            <input 
+                                                value={value.selectSubject !== '-1' ? value.subjectName : ""}
+                                                onChange={(e)=>changeData(e.target.value, data[idx].type, index)}/>
+                                            </span>
                                 }
                                 {
                                     (item.id === "textArea" ) && 
-                                        <span><textarea onChange={(e)=>changeData(e.target.value, data[idx].type)}/></span>
+                                        <span>
+                                            <textarea 
+                                                value={value.selectSubject !== '-1' ? value.subjectContents : ""}
+                                                onChange={(e)=>changeData(e.target.value, data[idx].type, index)}/></span>
                                 }
                                 {
                                     (item.id === "select" ) && 
                                     <span>
-                                        <select onChange={(e)=>changeData(e.target.value, data[idx].type)}>
-                                            <option key={-1} value={-1}>전체</option>
+                                        <select onChange={(e)=>changeData(e.target.value, data[idx].type, index)}>
+                                            <option key={-1} value={-1}>과업을 선택해주세요.</option>
                                             {
                                                 selectList.map((item)=> {
                                                     return (
@@ -49,24 +57,22 @@ const SingleDataTable = (props) => {
                                             {
                                                 teamList && 
                                                 teamList.map((item, idx) => {
-                                                    // return <span style={{display:'block'}} key={idx}>{Object.values(item)}<img src={Delete} alt="icon" onClick={()=>deleteTeam(idx, item)}/></span>
-
-                                                    return(
-                                                        item[idx] &&
-                                                            <span key={idx}>
-                                                                { 
-                                                                    item.map((team) => {
-                                                                        return Object.values(team.name)
-                                                                    })
-                                                                }
-                                                                <img src={Delete} alt="icon" onClick={()=>deleteTeam(idx, item)}/>
-                                                            </span>
-                                                    ) 
+                                                    // return <span style={{display:'block'}} key={idx}>{Object.values(item.name)}<img src={Delete} alt="icon" onClick={()=>deleteTeam(idx, item)}/></span>
+                                                    return (
+                                                        <span key={idx} style={{marginRight: 5}}>
+                                                            { 
+                                                                item.map((team) => {
+                                                                    return Object.values(team.name)
+                                                                })
+                                                            }
+                                                            <img src={Delete} alt="icon" onClick={()=>deleteTeam(idx, item, index)}/>
+                                                        </span>
+                                                    )
                                                 })
                                             }
                                             <button 
                                                 className="pagebutton"
-                                                onClick={()=>openModal()}>내 학생 리스트</button>
+                                                onClick={()=>openModal(index)}>내 학생 리스트</button>
                                         </span>
                                 }
                                 <span>{item.info}</span>
